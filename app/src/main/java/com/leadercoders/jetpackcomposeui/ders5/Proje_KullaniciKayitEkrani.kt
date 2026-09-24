105807ce12a892002207f3ce3c58c3bdea464482
AhmetCan Cuma Yıldırım <ahmetcancumayildirim@gmail.com>
2026-09-25 00:29:13 +0300

    Ders5 - Proje_KullaniciKayitEkrani v2

--- app/src/main/java/com/leadercoders/jetpackcomposeui/ders5/Proje_KullaniciKayitEkrani.kt
+++ app/src/main/java/com/leadercoders/jetpackcomposeui/ders5/Proje_KullaniciKayitEkrani.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
@@ -26,10 +25,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.TextButton
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -112,7 +112,8 @@ fun Proje_KullaniciKayitEkrani() {
         OutlinedTextField(
             value = sifre,
             onValueChange = { sifre = it },
-            label = { Text("Şifreniz") },
+            label = { Text("Şifre") },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth(),
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Password,
@@ -162,14 +163,14 @@ fun Proje_KullaniciKayitEkrani() {
             )
         }
 
-        Spacer(modifier = Modifier.height(100.dp))
+        Spacer(modifier = Modifier.height(50.dp))
 
-        Row(modifier = Modifier.padding(20.dp)) {
-            Text(text = "Zaten hesabınız var mı?", color = Color.Gray)
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)) {
+            Text(text = "Zaten hesabınız var mı?", color = Color.Gray, fontSize = 16.sp)
 
             TextButton(
                 onClick = {}, Modifier.padding()
-            ) { Text(text = "Giriş yap", color = Color(0xFF2196F3)) }
+            ) { Text(text = "Giriş yap", color = Color(0xFF2196F3), fontSize = 16.sp) }
         }
     }
 }
